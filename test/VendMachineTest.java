ef7f7d22fd6da22603ace85c05af7b040c466eab
Brian W <bweisbe2@illinois.edu>
2017-09-15 21:55:42 -0400

    refactored to make less redundancy

--- test/VendMachineTest.java
+++ test/VendMachineTest.java
@@ -5,10 +5,18 @@ import static org.junit.Assert.*;
 public class VendMachineTest {
 
     VendMachine vendMachine;
+    Coin quarter;
+    Coin nickel;
+    Coin dime;
+    Coin penny;
 
     @Before
     public void setUp() {
         vendMachine = new VendMachine();
+        quarter = new Coin(0.2, 0.955);
+        nickel = new Coin(0.176, 0.835);
+        dime = new Coin(0.08, 0.705);
+        penny = new Coin(0.088, 0.750);
     }
 
     @Test
@@ -18,21 +26,18 @@ public class VendMachineTest {
 
     @Test
     public void whenCoinIsInsertedItDisplaysTwentyFiveCents() {
-        Coin quarter = new Coin(0.2, 0.955);
         assertEquals("$0.25", vendMachine.insertCoin(quarter));
     }
 
     @Test
     public void whenValidCoinIsInsertedItDisplaysCorrectCoinValue() {
-        Coin nickel = new Coin(0.176, 0.835);
-        Coin dime = new Coin(0.08, 0.705);
         assertEquals("$0.05", vendMachine.insertCoin(nickel));
         assertEquals("$0.10", vendMachine.insertCoin(dime));
     }
 
     @Test
     public void whenInvalidCoinIsInsertedItRejects() {
-        Coin penny = new Coin(0.088, 0.750);
         assertEquals("INSERT COIN", vendMachine.insertCoin(penny));
     }
+
 }
